018f5c952183893fb30d3c86ba6cd683eec08aa4
ipodishima <marian.paul2@gmail.com>
2015-08-18 11:16:49 -0400

    Initial Commit

---
+++ WAAppRouter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WAAppRouter
+//
+//  Created by Marian Paul on 18/08/2015.
+//  Copyright (c) 2015 Wasappli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WAAppRouter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WAAppRouter
+//
+//  Created by Marian Paul on 18/08/2015.
+//  Copyright (c) 2015 Wasappli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
