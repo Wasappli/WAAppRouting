77783d3eb6da0f26d18a680fced7484d4ed2db82
ipodishima <marian.paul2@gmail.com>
2015-11-27 10:52:16 -0500

    Fixed an issue with iOS 7 compatibility thanks to @enfoiro Fix #12

--- Files/WAAppRouteMatcher.m
+++ Files/WAAppRouteMatcher.m
@@ -12,15 +12,8 @@
 @implementation WAAppRouteMatcher
 
 - (BOOL)matchesURL:(NSURL *)url fromPathPattern:(NSString *)pathPattern {
-    // Remove the parameters
-    NSURLComponents *urlComponents = [[NSURLComponents alloc] initWithURL:url resolvingAgainstBaseURL:NO];
-    urlComponents.query = nil;
     BOOL urlMatchesPattern = [self doesPathPattern:pathPattern
-                                      matchesRoute:urlComponents.string];
-    if (!urlMatchesPattern) {
-        urlMatchesPattern = [self doesPathPattern:pathPattern
-                                     matchesRoute:[self pathWithoutScheme:url]];
-    }
+                                      matchesRoute:[self pathWithoutScheme:url]];
     
     return urlMatchesPattern;
 }
